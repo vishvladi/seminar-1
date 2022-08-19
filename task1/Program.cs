9ed2438ea80c63455f5f30b5de56aed71cdc3bdb
Macbook Pro 15 <macbookpro@MacBook-Pro-Macbook.local>
2022-08-19 18:40:47 +0300

    Квадрат второго числа

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = int.Parse(Console.ReadLine());
+if (b * b == a)
+{
+  Console.WriteLine("Первое число квадрат второго числа");  
+}
+else {
+    Console.WriteLine("Нет");
+}
