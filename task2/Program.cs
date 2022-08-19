71896af9d430d16b6fdb26291ee01c59ed6795f2
Macbook Pro 15 <macbookpro@MacBook-Pro-Macbook.local>
2022-08-19 20:01:17 +0300

    weekdays

---
+++ task2/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Input a number from 1 to 7");
+int n = int.Parse(Console.ReadLine());
+int a = 1;
+int b = 2;
+int c = 3;
+int d = 4;
+int e = 5;
+int f = 6;
+int g = 7;
+if (n == a){
+Console.WriteLine("Monday");
+}
+if (n == b){
+Console.WriteLine("Tuesday");
+}
+if (n == c){
+Console.WriteLine("Wednesday");
+}
+if (n == d){
+Console.WriteLine("Thursday");
+}
+if (n == e){
+Console.WriteLine("Friday");
+}
+if (n == f){
+Console.WriteLine("Saturday");
+}
+if (n == g){
+Console.WriteLine("Sunday");
+}
