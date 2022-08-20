03c2c3b27b5fdab37b131da88319ec874aa78f57
Macbook Pro 15 <macbookpro@MacBook-Pro-Macbook.local>
2022-08-21 01:09:42 +0300

    третья цифра числа

---
+++ task11/Program.cs
@@ -0,0 +1,10 @@
+﻿int n = 100;
+int i = new Random().Next(1, 999);
+Console.WriteLine(i);
+int a = i % 10;
+if (i < n){
+    Console.WriteLine("Третья цифра отсутствует");
+}
+else {
+    Console.WriteLine(a + "третья цифра");
+}
