5f35b8f611739d9b8f3cc97b9fc7a8d0db010de1
Macbook Pro 15 <macbookpro@MacBook-Pro-Macbook.local>
2022-08-20 23:24:52 +0300

    Наибольшая цифра числа

---
+++ task8/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Input a number from 10 to 99");
+int n = int.Parse (Console.ReadLine());
+int a = n / 10;
+int b = n % 10;
+if (a > b){
+    Console.WriteLine(a + "- max");
+}
+else
+  {
+    Console.WriteLine(b + "- max");
+  }  
