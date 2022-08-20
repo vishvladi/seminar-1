24ca6db246368f058998e2d4c1b9c3ab45e2e76c
Macbook Pro 15 <macbookpro@MacBook-Pro-Macbook.local>
2022-08-20 15:15:56 +0300

    max of three numbers

---
+++ task3/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = new Random(). Next(1, 10);
+Console.WriteLine(a);
+int b = new Random(). Next(1, 10);
+Console.WriteLine(b);
+int c = new Random(). Next(1, 10);
+Console.WriteLine(c);
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+ {
+Console.Write("max =  ");
+Console.WriteLine(max);
+}
