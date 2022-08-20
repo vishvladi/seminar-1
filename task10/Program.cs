d49f46eb0b84db9e89049aba9e83972d2ebd7608
Macbook Pro 15 <macbookpro@MacBook-Pro-Macbook.local>
2022-08-21 00:42:53 +0300

    КРАТНОЕ

---
+++ task10/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите однозначное число");
+int n = int.Parse(Console.ReadLine());
+int a = new Random().Next (1, 100);
+Console.WriteLine(a);
+if (a % n == 0) {
+Console.WriteLine(a +" "+ "кратное");
+}
+else
+{
+    Console.WriteLine("остаток " + a % n);
+}
