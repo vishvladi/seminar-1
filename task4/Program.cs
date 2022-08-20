27061a2fb7ed503f4bc54c7f5b875c9ea2ffe780
Macbook Pro 15 <macbookpro@MacBook-Pro-Macbook.local>
2022-08-20 15:50:59 +0300

    Четное число

---
+++ task4/Program.cs
@@ -0,0 +1,9 @@
+﻿ Console.WriteLine("Input a number");
+ int a = int.Parse(Console.ReadLine());
+ if ( a%2 == 0){
+ Console.WriteLine(a + "чётное число");
+ }
+ else 
+ {
+    Console.WriteLine( a + "нечетное число");
+ }
